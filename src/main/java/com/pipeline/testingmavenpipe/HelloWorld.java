4d14baf7fcfed47de9c85d51734fbf8ea1fd4e41
abdelali-elhammadi-nw <abdelali.el-hammadi@nimbleways.com>
2023-06-09 19:15:37 +0100

    pleaaaaaaase

---
+++ src/main/java/com/pipeline/testingmavenpipe/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.pipeline.testingmavenpipe;
+
+public class HelloWorld {
+
+    public static void sayHello(){
+        System.out.println("HEEEEEEEEELOO");
+    }
+}
---
+++ src/test/java/com/pipeline/testingmavenpipe/TestingMavenPipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipeline.testingmavenpipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingMavenPipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

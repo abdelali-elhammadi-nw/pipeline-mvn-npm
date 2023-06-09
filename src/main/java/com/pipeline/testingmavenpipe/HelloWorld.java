25893b1fa12f8d34d8ebc994263846fa1a360129
abdelali-elhammadi-nw <abdelali.el-hammadi@nimbleways.com>
2023-06-09 20:54:30 +0100

    yarbi tkhdam

--- src/main/java/com/pipeline/testingmavenpipe/HelloWorld.java
+++ src/main/java/com/pipeline/testingmavenpipe/HelloWorld.java
@@ -3,6 +3,6 @@ package com.pipeline.testingmavenpipe;
 public class HelloWorld {
 
     public static void sayHello(){
-        System.out.println("HEEEEEEEEELOO");
+        System.out.println("HEEEEEEEEELOeeeO");
     }
 }
